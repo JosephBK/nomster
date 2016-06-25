85837192ed8d866892a678c17dad58a98b8281a9
rormuc-17 <bazaare.joseph@gmail.com>
2016-06-25 17:51:59 +0200

    Connect tables with user_id

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,13 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places=Place.all
+    puts params
+    if params[:q]
+      @places=Place.where('name ilike ?',"%#{params[:q]}%")
+    else
+      @places=Place.all
+    end
   end
 
   def new
@@ -9,10 +15,10 @@ class PlacesController < ApplicationController
   end
 
   def create
-  @place = Place.create(place_params)
+  @place = current_user.places.create!(place_params)
   if @place.invalid?
     flash[:error] ='<strong>Descriptions must be 3 to 140 characters long</strong>'
-     render :new 
+    render :new 
   else    
   redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625125715) do
+ActiveRecord::Schema.define(version: 20160625152542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,11 @@ ActiveRecord::Schema.define(version: 20160625125715) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "places", ["name"], name: "index_places_on_name", using: :btree
+  add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
