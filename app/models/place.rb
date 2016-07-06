deba08005acc31a9351f0a0bbc115ac0a05d1d1a
rormuc-17 <bazaare.joseph@gmail.com>
2016-07-06 14:55:12 +0200

    Add Validation Rules

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true, length {minimum:3}
+  validates :name, presence: true, length: {minimum:3}
   validates :address, presence: true
   validates :description, presence: true, length:{maximum:140, minimum:3}
 end
